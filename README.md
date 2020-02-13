9ec26dd90cab0e2019c9f2f1511c5db74324f75b
jacob.romero <jacob.romero@gentex.com>
2020-02-13 11:43:29 -0500

    npm setup

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# node-box
+# Node Box
+
+Quick way to create an express app for static html/js sites with some extra features.
+
+**Install**
+
+`npm install --save @klouddydev/node-box`
+
+**Sample Usage**
+
+```js
+
+```
--- example.js
+++ example.js
@@ -2,7 +2,7 @@
  *
  * This is an example of how to use node box
  */
-const nodeBox = require('./dist/index.js');
+const nodeBox = require('@klouddydev/node-boxy');
 var a = new nodeBox.NodeBox({filename: './config.yml'});
 
 a.logger.warn('logging warn message');
